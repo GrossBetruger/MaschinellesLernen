45f6beb9d6b25243c0ebed7e7ad072e1662b6408
koi <koriatoren@gmail.com>
2017-11-12 23:32:50 +0200

    added some currencies and saved one result

--- SentimentAnalysis/currency_vibe_analyzer.py
+++ SentimentAnalysis/currency_vibe_analyzer.py
@@ -12,9 +12,13 @@ LITECOIN_SUBREDDIT = 'https://www.reddit.com/r/litecoin/'
 MONERO_SUBREDDIT = 'https://www.reddit.com/r/Monero/'
 BITCOIN_SUBREDDIT = 'https://www.reddit.com/r/Bitcoin/'
 BITCOIN_CASH_SUBREDDIT = 'https://www.reddit.com/r/Bitcoincash/'
+ETHEREUM_SUBREDDIT = 'https://www.reddit.com/r/ethereum/'
 LISK_CASH_SUBREDDIT = 'https://www.reddit.com/r/Lisk/'
 AUGUR_CASH_SUBREDDIT = 'https://www.reddit.com/r/Augur/'
 NEO_SUBREDDIT = 'https://www.reddit.com/r/NEO/'
+STEEM_SUBREDDIT = 'https://www.reddit.com/r/steem/'
+ETHEREUM_CLASSIC_SUBREDDIT = 'https://www.reddit.com/r/EthereumClassic/'
+DOGECOIN_CLASSIC_SUBREDDIT = 'https://www.reddit.com/r/dogecoin/'
 
 # blank = """<a class="title may-blank outbound" data-event-action="title" href="https://coincodex.com/crypto/dash/?view1" tabindex="1" data-href-url="https://coincodex.com/crypto/dash/?view1" data-outbound-url="https://out.reddit.com/t3_7cgnr8?url=https%3A%2F%2Fcoincodex.com%2Fcrypto%2Fdash%2F%3Fview1&amp;token=AQAAYZsIWspgFRZ8Qxy2e_sWXCrmq1B8yf3L-XHFNZzxErTt4_-R&amp;app_name=reddit.com" data-outbound-expiration="1510513505000" rel="nofollow">🚀 Dash up 25% in the last hour! Any news or just cos of BCH cooling down and money flowing back to alts?</a>"""
 
@@ -27,7 +31,9 @@ def get_titles(subreddit_url):
     return titles
 
 
-def average_sentiment(text, ignore_zeros=True):
+def average_sentiment(text, ignore_zeros=True, negative_only=False):
+    if negative_only:
+        return mean([x for x in [analyze_sentiment(line) for line in text] if x < 0])
     if ignore_zeros:
         return mean([x for x in [analyze_sentiment(line) for line in text] if x != 0])
     return mean([analyze_sentiment(line) for line in text])
@@ -58,3 +64,11 @@ if __name__ == '__main__':
     titles = get_titles(NEO_SUBREDDIT)
     print "Neo Sentiment:", average_sentiment(titles)
 
+    titles = get_titles(STEEM_SUBREDDIT)
+    print "Steem Sentiment:", average_sentiment(titles)
+
+    titles = get_titles(ETHEREUM_SUBREDDIT)
+    print "Ethereum Sentiment:", average_sentiment(titles)
+
+    titles = get_titles(ETHEREUM_CLASSIC_SUBREDDIT)
+    print "Ethereum classic Sentiment:", average_sentiment(titles)

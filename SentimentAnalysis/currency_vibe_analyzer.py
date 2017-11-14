71fea53f969419d1fef64f04555816bd7d94fe0e
koi <koriatoren@gmail.com>
2017-11-15 01:05:56 +0200

    added fundementals polling

--- SentimentAnalysis/currency_vibe_analyzer.py
+++ SentimentAnalysis/currency_vibe_analyzer.py
@@ -5,7 +5,11 @@ from sentiment import analyze_sentiment, analyze_subjectivity
 import requests
 import re
 from numpy import mean
+from xmljson import badgerfish as bf
+from xml.etree.ElementTree import fromstring
 
+
+COINGECKO_MAIN = 'https://www.coingecko.com/en'
 
 DASH_SUBREDDIT = 'https://www.reddit.com/r/dashpay/'
 LITECOIN_SUBREDDIT = 'https://www.reddit.com/r/litecoin/'
@@ -20,16 +24,43 @@ STEEM_SUBREDDIT = 'https://www.reddit.com/r/steem/'
 ETHEREUM_CLASSIC_SUBREDDIT = 'https://www.reddit.com/r/EthereumClassic/'
 DOGECOIN_CLASSIC_SUBREDDIT = 'https://www.reddit.com/r/dogecoin/'
 RIPPLE_CLASSIC_SUBREDDIT = 'https://www.reddit.com/r/Ripple/'
+TETHER_SUBREDDIT = 'https://www.reddit.com/r/Tether/'
+BANCOR_SUBREDDIT = 'https://www.reddit.com/r/Bancor/'
 
 NEXT_PAGE_PATTERN = '\?count=\d+&amp;after=[^"]+'
 SECOND_NEXT_PAGE_PATTERN = '\?amp=&amp;count=\d+&amp;after=[^"]+'
 
 
-def get_titles(subreddit_url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    res = requests.get(subreddit_url, headers=headers)
+def scrap_text(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    res = requests.get(url, headers=headers)
     res.raise_for_status()
-    raw_text = res.text
+    return res.text
+
+
+def get_coin_fundementals():
+    raw_text = scrap_text(COINGECKO_MAIN).replace("\n", "")
+    rows = re.findall("<tr>.+?</tr>", raw_text)
+    for row in rows:
+        # if "monero" in row:
+        try:
+            name = re.findall(r"crypto-symbol text-muted'>(.+?)</span>", row)[0]
+            hash_algo = re.findall(r"<small>(.+?)</small>", row)[0]
+            print "NAME:", name
+            print "HASH ALGO:", hash_algo
+            print
+        except IndexError:
+            pass
+
+
+
+def parse_xml(raw):
+    return bf.data(fromstring(raw))
+
+
+def get_titles(subreddit_url):
+    raw_text = scrap_text(subreddit_url)
     titles = re.findall('<a class="title.+?>(.+?)<', raw_text)
     next_url_suffix = re.findall(NEXT_PAGE_PATTERN, raw_text)
     next_url_suffix2 = re.findall(SECOND_NEXT_PAGE_PATTERN, raw_text)
@@ -74,6 +105,8 @@ def full_analysis(currency_name, subreddit_url):
 
 
 if __name__ == '__main__':
+    get_coin_fundementals()
+    quit()
     full_analysis("Dash", DASH_SUBREDDIT)
     full_analysis("Litecoin", LITECOIN_SUBREDDIT)
     full_analysis("Monero", MONERO_SUBREDDIT)
@@ -86,3 +119,5 @@ if __name__ == '__main__':
     full_analysis("Ethereum", ETHEREUM_SUBREDDIT)
     full_analysis("Ethereum Classic", ETHEREUM_CLASSIC_SUBREDDIT)
     full_analysis("Ripple", RIPPLE_CLASSIC_SUBREDDIT)
+    full_analysis("Tether", TETHER_SUBREDDIT)
+    full_analysis("Bancor", BANCOR_SUBREDDIT)
